59f924695f0a62d43fe22ff77e9c45b02e9fb714
Egor Gorokh <gorokhegor@gmail.com>
2023-08-22 20:09:27 +0300

    first commit

---
+++ resources/views/admin/categories/create.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.layouts.main')
+@section('content')
+
+
+<div class="content-wrapper">
+
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                     
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                  
+                    <div class="col-12">
+                        <h2>
+                        Добавление категории
+                        </h2>
+           
+            <form action="{{route('admin.category.store')}}" method="POST" class="w-25">
+               @csrf
+               <div class="form-group">
+                 <input type="text" name="title" class="form-control"  placeholder="название"></div>
+                 @error('title')
+<div class="text-danger">Это поле необходимо заполнить</div>
+                 @enderror
+                 <input type="submit" class="btn btn-primary" value="Добавить">
+            </form>
+             </div>
+                    <!-- ./col -->
+                </div>
+                <!-- /.row -->
+                <!-- Main row -->
+              
+                <!-- /.row (main row) -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+
+
+</div>
+
+    @endsection
---
+++ resources/views/admin/tag/index.blade.php
@@ -0,0 +1,92 @@
+@extends('admin.layouts.main')
+@section('content')
+
+
+<div class="content-wrapper">
+
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Теги</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Dashboard v1</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Small boxes (Stat box) -->
+                <div class="row">
+                    <div class="col-2 mb-3" >
+                    <a href="{{route('admin.tag.create')}}" class="btn btn-block btn-primary">добавить</a>
+                    </div>
+                    <div class="col-12">
+             
+             <div class="card">
+              
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                  <thead>
+                   
+                    <tr>
+                      <th>ID</th>
+                      <th>Название</th>
+                      <th class="text-center"colspan="3">действия</th>
+                   
+                   
+                    </tr>
+                  </thead>
+                  <tbody>
+
+                  @foreach($tags as $tag)
+                    <tr>
+                      <td>{{$tag->id}}</td>
+                      <td>{{$tag->title}}</td>
+                      <th class="text-center"><a href="{{route('admin.tag.show',$tag->id)}}"><i class="far fa-eye"></i></a></th>
+                      <th class="text-center"><a href="{{route('admin.tag.edit',$tag->id)}}"><i class="far fa-pencil-alt"></i></a></th>
+                      <th class="text-center">
+                        <form action="{{route('admin.tag.delete',$tag->id)}}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="border-0 bg-transparent">
+                            <i class="far fa-trash text-danger" role="button"></i>
+                            </button>
+                 
+                        </form>
+                    </th>
+                 
+                    </tr>
+                    
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+             </div>
+                    <!-- ./col -->
+                </div>
+                <!-- /.row -->
+                <!-- Main row -->
+              
+                <!-- /.row (main row) -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+
+
+</div>
+
+    @endsection
